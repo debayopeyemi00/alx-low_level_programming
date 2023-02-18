be1352ddb2c80afa9f7af21c3948f60a8a6e85a5
debayopeyemi00 <111307114+debayopeyemi00@users.noreply.github.com>
2023-02-18 03:20:54 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,15 @@ n = rand() -RAND_MAX / 2;
 printf("Last digit of %i is %i and is ", n, (n % 10));
 if ((n % 10) == 0)
 {
-printf("0
-);
+printf("0\n");
 }
 else if ((n % 10) > 5)
 {
-printf("greater than 5
-");
+printf("greater than 5\n");
 }
 else
 {
-print("less than 6 and not 0
-");
+print("less than 6 and not 0\n");
 }
 return (0);
 }
