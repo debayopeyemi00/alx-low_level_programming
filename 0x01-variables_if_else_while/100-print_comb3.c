1678ec88a4ae4c27e9301acb4c5a3c2867c832a2
debayopeyemi00 <111307114+debayopeyemi00@users.noreply.github.com>
2023-02-18 05:08:17 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,19 @@ int main(void)
 {
 int ones;
 int tens;
-for (tens = 0; tens <= 9; tens++)/*prints tens*/
+for (tens = '0'; tens <= '9'; tens++)/*prints tens*/
+{
+for (ones = (tens + 1); ones <= '9'; ones++)
 {
 putchar(tens);
 putchar(ones);
-if (tens !=8 || ones !=9)/*print commas*/
+if (tens != '8' || ones != '9')/*print commas*/
 {
-putchar(,);
-putchar( );
+putchar(',');
+putchar(' ');
 }
 }
 }
-putchar(n);
+putchar('\n);
 return (0);
 }
