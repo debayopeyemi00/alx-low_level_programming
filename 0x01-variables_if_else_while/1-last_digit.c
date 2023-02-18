6cc2a7feed7750558367f0b614d08c5e5ed3107e
debayopeyemi00 <debayopeyemi00@gmail.com>
2023-02-17 18:17:22 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,19 +9,22 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAX / 2;
+n = rand() -RAND_MAX / 2;
 printf("Last digit of %i is %i and is ", n, (n % 10));
 if ((n % 10) == 0)
 {
-printf("0\n");
+printf("0
+);
 }
 else if ((n % 10) > 5)
 {
-printf("greater than 5\n");
+printf("greater than 5
+");
 }
 else
 {
-printf("less than 6 and not 0\n");
+print("less than 6 and not 0
+");
 }
 return (0);
 }
