fa25fe9116409cd86fe1112cd15a7a45c5cf6865
debayopeyemi00 <debayopeyemi00@gmail.com>
2023-02-17 19:34:11 -0800

    combo commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main- prints hexadecimal base
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = 0;
+int a_to_f = a;
+while (n <= 9)/*prints 0-9*/
+{
+putchar(n);
+n++;
+}
+while (a_to_f <= f)/*prints a-f*/
+{
+putchar(a_to_f);
+a_to_f++;
+}
+putchar(n);
+return (0);
+}
