d1afef417288b204e1f50eb52d2980c0ccd9a840
debayopeyemi00 <111307114+debayopeyemi00@users.noreply.github.com>
2023-02-18 01:59:11 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if ((n % 10) == 0)
 	{
-		printf("0\r\n");
+		printf("0\n");
 	}
 	else if ((n % 10) > 5)
 	{
-		printf("greater than 5\r\n");
+		printf("greater than 5\n");
 	}
 	else
 	{
-		printf("less than 6 and not 0\r\n");
+		printf("less than 6 and not 0\n");
 	}
 
 	return (0);
