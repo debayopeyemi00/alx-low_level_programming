5086eee153660e740d6e6e6bb425b7b8c73807d9
debayopeyemi00 <debayopeyemi00@gmail.com>
2023-02-21 23:47:26 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include main.h
+/**
+*_abs- prints absolute number
+*@i: integer to be converted
+*Return: absolute value of the input
+*/
+int _abs(int i)
+{
+if (i < 0)
+i = i * -1;
+return (i);
+}
