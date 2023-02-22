957201fd8d760aac67c69c3989ef86d09d81e64c
debayopeyemi00 <debayopeyemi00@gmail.com>
2023-02-21 23:05:49 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+**/
+*_isalpha- checks for alpbabet
+*@c: character to check
+*Return: 1 if c is alphabet (lower or upper), 0 otherwise
+*/
+int _isalpha(int c)
+{
+if ((c >= a && c <= z) || (c >= A && c <= Z))
+return (1);
+else
+return (0);
+}
