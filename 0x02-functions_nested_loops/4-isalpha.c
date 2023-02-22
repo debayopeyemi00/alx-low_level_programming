df52e000cd8edd29e4f8785d87a4dcfefe6da6f7
debayopeyemi00 <111307114+debayopeyemi00@users.noreply.github.com>
2023-02-22 08:07:55 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
-**/
+/**
 *_isalpha- checks for alpbabet
 *@c: character to check
 *Return: 1 if c is alphabet (lower or upper), 0 otherwise
 */
 int _isalpha(int c)
 {
-if ((c >= a && c <= z) || (c >= A && c <= Z))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
