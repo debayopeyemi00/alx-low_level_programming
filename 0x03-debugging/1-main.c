2966796e029f69a3223f6b88df448d83a75d4c63
debayopeyemi00 <111307114+debayopeyemi00@users.noreply.github.com>
2023-02-23 15:53:09 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,14 +6,12 @@
 int main(void)
 {
 int i;
-printf("Infinte loop incoming :(
-");
+printf("Infinte loop incoming :(\n");
 i = 0;
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
 /*}*/
-printf("Infinite loop avoided! \o/
-");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
