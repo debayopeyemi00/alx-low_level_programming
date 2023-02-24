1cd90962269251a0936bb27cee556381df7e9839
debayopeyemi00 <111307114+debayopeyemi00@users.noreply.github.com>
2023-02-24 09:45:42 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-*print_most_numbers - print 0 to 9
+*print_most_numbers - print the numbers from 0 to 9
+*Description: prints the numbers excluding 2 and 4
 *Return: The numbers from 0 to 9
 */
 void print_most_numbers(void)
