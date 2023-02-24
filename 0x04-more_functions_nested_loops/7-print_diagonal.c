e9f70fa2ab020c6ffe05c664fcd2abffde187375
debayopeyemi00 <debayopeyemi00@gmail.com>
2023-02-24 01:15:23 -0800

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_diagonal - draws a diagonal line
+*@n: number of time to print
+*Return: empty
+*/
+void print_diagonal(int n)
+{
+int x, y;
+if (n <= 0)
+{
+_putchar(n);
+}
+ele
+{
+for (x = 0; x < n; x++)
+{
+foe (y = 0; y < x; y++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar(n);
+}
+}
