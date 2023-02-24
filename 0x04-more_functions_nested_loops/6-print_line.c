3b07a6ee3eb5976f19fdf30330086454bb202bea
debayopeyemi00 <111307114+debayopeyemi00@users.noreply.github.com>
2023-02-24 10:07:53 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,7 @@ void print_line(int n)
 int x;
 if (n <= 0)
 {
-_putchar(n);
+_putchar('\n');
 }
 else
 {
@@ -17,6 +17,6 @@ for (x = 0; x < n; x++)
 {
 _putchar(95);
 }
-_putchar(n);
+_putchar('\n');
 }
 }
